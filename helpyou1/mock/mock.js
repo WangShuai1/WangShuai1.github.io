2dbb9b4fca70c1a03195157ebb0950d003778191
1414798436 <1414798436@qq.com>
2016-06-27 21:09:22 +0800

    helpyou1 upload

---
+++ helpyou1/mock/mock.js
@@ -0,0 +1,12 @@
+module.exports = {
+	rules: [{
+		pattern: /\/api\/getLivelist.php\?rtype=origin$/,
+		respondwith: './livelist.json'
+	}, {
+		pattern: /\/api\/getLivelist.php\?rtype=more$/,
+		respondwith: './morelist.json'
+	}, {
+		pattern: /\/api\/getLivelist.php\?rtype=refresh$/,
+		respondwith: './refreshlist.json'
+	}]
+}
